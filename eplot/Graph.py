9520ac6c0be92007db92fa251baa2fc299576b2c
EosinY <muska5312187@gmail.com>
2024-10-28 01:30:24 +0900

    defined types and consts

--- eplot/Graph.py
+++ eplot/Graph.py
@@ -30,11 +30,17 @@ class LineType(Enum):
 
 @unique
 class AxisType(Enum):
-    Linear = "0"
-    Log = "1"
+    Linear = "linear"
+    Log = "log"
 
     def Value(e):
         return e.value
+
+    def ToEnum(e: str):
+        if e == "1":
+            return AxisType.Linear
+        elif e == "2":
+            return AxisType.Log
 
 
 class XY_PlotEntry:
@@ -71,3 +77,43 @@ class XY2_PlotEntry(XY_PlotEntry):
         super().__init__(x_data, y_data, name, groupid=groupid, color=color, point=point, line=line)
 
         self.y2_data = y2_data
+
+
+class GraphMaker():
+    entries_xy: list[XY_PlotEntry]
+    entries_xy2: list[XY2_PlotEntry]
+
+    _grid: tuple[bool] = (True, True, False)
+    _margin: tuple[float] = (0.125, 0.875, 0.125, 0.9)
+
+    def __init__(self, path: str):
+        self.path = path
+
+    def _Str2AxisType(i: int) -> Union[tuple[AxisType, AxisType], tuple[AxisType, AxisType, AxisType]]:
+        al = []
+        for s in str(i):
+            al.append(AxisType.ToEnum(s))
+
+        return tuple(al)
+
+    def Plot_XY(
+            self,
+            axisname: tuple[str, str],
+            x_region: tuple[float, float] = (None, None),
+            y_region: tuple[float, float] = (None, None),
+            axistype: Union[int, tuple[AxisType, AxisType]] = 11,
+            ticks: Union[list[float], list[Dict[float, str]]] = None,
+            legposition=None):
+
+        return
+
+    def Plot_XY2(
+            self,
+            axisname: tuple[str, str, str],
+            x_region: tuple[float, float] = (None, None),
+            y_region: tuple[float, float] = (None, None),
+            y2_region: tuple[float, float] = (None, None),
+            axistype: Union[int, tuple[AxisType, AxisType, AxisType]] = 111,
+            ticks: Union[list[float], list[Dict[float, str]]] = None,
+            legposition=None):
+        return
