7ea24e226ceb11fbeba06bdd796e1bbe42c30a06
EosinY <77866829+EosinY@users.noreply.github.com>
2025-08-01 17:45:47 +0900

    add making color variant method

--- eplot/Colors.py
+++ eplot/Colors.py
@@ -30,3 +30,22 @@ muted_colors: Final[List[str]] = [
     "#AA4499",
     "#DDDDDD"
 ]
+
+
+def get_color_variant(color_l: list[str], count: int, start: int = 0, sub_val: tuple[int, int, int] = (0x30, 0x30, 0x30)):
+    color_l = color_l[start:count]
+    mcolor_l = []
+
+    color_nums = [int(s.lstrip("#"), 16) for s in color_l]
+    mask_shifts = [16, 8, 0]
+    for color in color_nums:
+        color_rgb = [(color & (0xFF << i)) >> i for i in mask_shifts]
+        mod_clrgb = [color_rgb[i] - sub_val[i] if color_rgb[i] - sub_val[i] >= 0 else 0 for i in range(len(color_rgb))]
+        mcolor = 0
+        for i in range(len(mod_clrgb)):
+            c = mod_clrgb[i]
+            mcolor |= c << mask_shifts[i]
+
+        mcolor_l.append("#" + hex(mcolor).lstrip("0x"))
+
+    return color_l + mcolor_l
